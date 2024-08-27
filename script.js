05b2d004430d13f63016c1e7b8128f2cae6f304b
Aman Patel <amanpatel3062005@gmail.com>
2024-08-28 01:15:28 +0530

    11th commit

--- script.js
+++ script.js
@@ -36,11 +36,11 @@ function page1animation() {
         duration: 0.6,
         stagger: 0.2
     });
-    tl.from("#butterfly,#butterfly2,#cloud",{
+    tl.from("#butterfly,#butterfly2,#butterfly4,#cloud", {
         opacity: 0,
         duration: 0.7,
     });
-   
+
 }
 
 page1animation();
@@ -70,150 +70,165 @@ function themeFunction() {
 themeFunction();
 
 {
-const suggestion = [
-    "Javascript",
-    "Web development",
-    "GreenSock",
-    "3d Website"
-];
+    const suggestion = [
+        "Peanut Butter",
+        "Web development",
+        "GreenSock",
+        "3d Website"
+    ];
 
-const inputbox = document.querySelector('#input-search-box');
-const searchResult = document.querySelector(".search-result");
+    const inputbox = document.querySelector('#input-search-box');
+    const searchResult = document.querySelector(".search-result");
 
-inputbox.addEventListener("keyup", function (data) {
-    let userData = data.target.value;
-    let filterData = [];
+    inputbox.addEventListener("keyup", function (data) {    
+        let userData = data.target.value;
+        let filterData = [];
 
-    if (userData) {
-        filterData = suggestion.filter((data) => {
-            return data.toLowerCase().includes(userData.toLowerCase());
-        });
-        filterData = filterData.map((data) => {
-            return data = '<li onclick="selectedList(this)">' + data + '</li>';
-        });
+        if (userData) {
+            filterData = suggestion.filter((data) => {
+                return data.toLowerCase().includes(userData.toLowerCase());
+            });
+            filterData = filterData.map((data) => {
+                return data = '<li onclick="selectedList(this)">' + data + '</li>';
+            });
+        }
+
+        displaySuggestion(filterData);
+
+        if (!filterData.length) {
+            searchResult.innerHTML = '';
+        }
+
+        if (data.key === "Enter") {
+            const selectedValue = inputbox.value.toLowerCase().trim();
+
+
+            const isMatch = suggestion.some(s => s.toLowerCase() === selectedValue);
+
+            if (isMatch) {
+
+                window.location.href = "./product-page/product.html";
+            } else {
+ 
+                alert("No data found");
+            }
+        }
+    });
+
+    function displaySuggestion(list) {
+        let dataList;
+        if (!list.length) {
+            dataList = '<li>' + inputbox.value + '</li>';
+        } else {
+            dataList = list.join('');
+        }
+        searchResult.innerHTML = dataList;
     }
 
-    displaySuggestion(filterData);
+    function selectedList(list) {
+        inputbox.value = list.innerHTML;
+        searchResult.innerHTML = "";
 
-    if (!filterData.length) {
-        searchResult.innerHTML = '';
-    }
+        const selectedValue = list.innerHTML.toLowerCase().trim();
 
-    // Check if Enter key is pressed
-    if (data.key === "Enter") {
-        const selectedValue = inputbox.value.toLowerCase().trim();
+        if (selectedValue == "peanut butter" ||
+            selectedValue == "web development" ||
+            selectedValue == "greensock" ||
+            selectedValue == "3d website") {
 
-        // Check if the entered value matches any suggestion
-        const isMatch = suggestion.some(s => s.toLowerCase() === selectedValue);
 
-        if (isMatch) {
-            // Redirect to product.html if a valid match is found
             window.location.href = "./product-page/product.html";
         } else {
-            // Alert if no matching data is found
-            alert("No data found");
+            alert("No page available for the selected suggestion.");
         }
     }
-});
-
-function displaySuggestion(list) {
-    let dataList;
-    if (!list.length) {
-        dataList = '<li>' + inputbox.value + '</li>';
-    } else {
-        dataList = list.join('');
-    }
-    searchResult.innerHTML = dataList;
 }
 
-function selectedList(list) {
-    inputbox.value = list.innerHTML;
-    searchResult.innerHTML = "";
 
-    const selectedValue = list.innerHTML.toLowerCase().trim();
+function butterfly(){
 
-    if (selectedValue === "javascript" ||
-        selectedValue === "web development" ||
-        selectedValue === "greensock" ||
-        selectedValue === "3d website") {
 
-        // Redirect to product.html when a suggestion is clicked
-        window.location.href = "./product-page/product.html";
-    } else {
-        alert("No page available for the selected suggestion.");
-    }
-}
-}
+var b1 = gsap.timeline({repeat: -1});
 
-var b1 = gsap.timeline({ repeat: -1 });
-
-b1
-    .to("#butterfly", {
+b1.to("#butterfly", {
         scaleX: 0.3,
-        duration: 0.5, 
-        yoyo: true,    
+        duration: 0.5,
+        yoyo: true,
         ease: "linear",
         repeat: -1
     })
     .to("#butterfly", {
-        x: "200vh", 
-        duration: 20, 
+        x: "200vh",
+        duration: 20,
         ease: "linear",
-        repeat:-1
-    }, butterfly) 
+        repeat: -1
+    }, butterfly)
     .to("#butterfly", {
-        y: "-100px",  
+        y: "-100px",
         duration: 1.25,
-        yoyo: true,   
-        repeat: -1,   
+        yoyo: true,
+        repeat: -1,
         ease: "sine.inOut",
-    }, butterfly); 
+    }, butterfly);
 
-    var b2 = gsap.timeline({ repeat: -1 });
-
-b2
-    .to("#butterfly2", {
+var b2 = gsap.timeline({repeat: -1});
+b2.to("#butterfly2", {
         scaleX: 0.3,
-        duration: 0.5, 
-        yoyo: true,    
+        duration: 0.5,
+        yoyo: true,
         ease: "linear",
         repeat: -1
     })
     .from("#butterfly2", {
-        x: "200vh", 
-        duration: 25, 
+        x: "200vh",
+        duration: 25,
         ease: "linear",
-        repeat:-1
-    }, butterfly) 
+        repeat: -1
+    }, butterfly)
     .to("#butterfly2", {
-        y: "300px",  
+        y: "350px",
         duration: 1.25,
-        yoyo: true,   
-        repeat: -1,   
-        ease: "sine.inOut", 
-    }, butterfly); 
+        yoyo: true,
+        repeat: -1,
+        ease: "sine.inOut",
+    }, butterfly);
+gsap.to("#cloud", {
+    x: "200vh",
+    duration: 25,
+    ease: "linear",
+    repeat: -1
+});
+gsap.to("#cloud2", {
+    x: "200vh",
+    duration: 30,
+    ease: "linear",
+    repeat: -1,
+    delay: 2
+});
+gsap.to("#butterfly3", {
+    scaleX: 0.3,
+    duration: 0.7,
+    ease: "linear",
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut"
+});
+gsap.to("#butterfly4", {
+    scaleX: 0.3,
+    duration: 1,
+    ease: "linear",
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut"
+});
+gsap.to("#butterfly5", {
+    scaleX: 0.3,
+    duration: 1,
+    ease: "linear",
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut"
+});
+}
 
-
-
-
-    gsap.to("#cloud",{
-        x: "200vh", 
-        duration: 25, 
-        ease: "linear",
-        repeat:-1
-    });
-    gsap.to("#cloud2",{
-        x: "200vh", 
-        duration: 30, 
-        ease: "linear",
-        repeat:-1,
-        delay:2
-    });
-    gsap.to("#butterfly3",{
-        scaleX:0.3, 
-        duration: 0.7   , 
-        ease: "linear",
-        repeat:-1,
-        yoyo: true, 
-        ease:"sine.inOut"
-    });
+butterfly();
